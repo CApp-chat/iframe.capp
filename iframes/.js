4b1498057ea4eeeb1198cf18e2cea794f7f355f5
CApp-chat <78289417+CApp-chat@users.noreply.github.com>
2021-03-08 21:30:21 +0530

    Create .js

---
+++ iframes/.js
@@ -0,0 +1,6 @@
+var allEmbeds = document.querySelectorAll('embed .capp-elem')
+for(ems in allEmbed){
+ current =  allEmbed[ems];
+ ChatroomId = current.dataset.chatid;
+ apiKey = provider.user[password].key;
+}
